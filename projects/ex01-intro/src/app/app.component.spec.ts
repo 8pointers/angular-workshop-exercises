12703c5b4a9c3eaf8f1135a6ef2bc0716ccc50f2
Damjan Vujnovic <damjan@samuraiprinciple.com>
2018-12-16 11:54:31 +0000

    update

--- projects/ex01-intro/src/app/app.component.spec.ts
+++ projects/ex01-intro/src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -16,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-workshop-exercises'`, () => {
+  it(`should have 'World' as name`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-workshop-exercises');
+    expect(app.name).toEqual('World');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render Hello World! in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-workshop-exercises!');
+    expect(compiled.querySelector('h1').textContent).toContain('Hello World!');
   });
 });
