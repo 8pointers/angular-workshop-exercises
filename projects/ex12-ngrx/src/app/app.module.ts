61eda541fd15f3c565ab826770ff6b621f69e5ad
Damjan Vujnovic <damjan@samuraiprinciple.com>
2019-01-30 16:45:13 +0100

    update

--- projects/ex12-ngrx/src/app/app.module.ts
+++ projects/ex12-ngrx/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { counterReducer } from './counter/counter.reducer';
-import { PatientsModule } from './patients/patients.module';
 
 @NgModule({
   declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule, StoreModule.forRoot({ counter: counterReducer }), PatientsModule],
+  imports: [BrowserModule, StoreModule.forRoot({ counter: counterReducer })],
   providers: [],
   bootstrap: [AppComponent]
 })
