f4e51445b0cb16747c296363693373d19a3ca145
Damjan Vujnovic <damjan@samuraiprinciple.com>
2019-06-27 17:15:57 +0100

    update

--- projects/ex10-rxjs/src/main.ts
+++ projects/ex10-rxjs/src/main.ts
@@ -1,8 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import './app/diy';
-import './app/rxjs';
 import { environment } from './environments/environment';
 
 if (environment.production) {
