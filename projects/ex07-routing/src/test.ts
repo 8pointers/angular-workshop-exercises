df3ea01a20de649bc16883c234ea384be1778859
Damjan Vujnovic <damjan@samuraiprinciple.com>
2020-06-20 14:20:22 +0100

    update

--- projects/ex07-routing/src/test.ts
+++ projects/ex07-routing/src/test.ts
@@ -4,7 +4,16 @@ import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import 'zone.js/dist/zone-testing';
 
-declare const require: any;
+declare const require: {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
+    keys(): string[];
+    <T>(id: string): T;
+  };
+};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
