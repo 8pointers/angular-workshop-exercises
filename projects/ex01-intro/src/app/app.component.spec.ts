d7ae3ebc87d53e1825791e1579f86adae7ea310b
Damjan Vujnovic <damjan@samuraiprinciple.com>
2020-06-21 15:59:21 +0100

    update

---
+++ projects/ex01-intro/src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
+  }));
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+  it(`should have as name 'World'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.name).toEqual('World');
+  });
+  it('should render Hello World!', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Hello World!');
+  });
+});
