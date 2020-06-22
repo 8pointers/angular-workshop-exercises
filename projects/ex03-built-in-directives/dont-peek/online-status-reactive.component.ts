149f604830238a1cf2933897e887922891516441
Damjan Vujnovic <damjan@samuraiprinciple.com>
2020-06-22 15:38:21 +0100

    update

--- projects/ex03-built-in-directives/dont-peek/online-status-reactive.component.ts
+++ projects/ex03-built-in-directives/dont-peek/online-status-reactive.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { fromEvent, merge } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-online-status',
   template: '{{(isOnline$ | async) ? "😀" : "🤕"}}'
 })
 export class OnlineStatusComponent {
-  isOnline$ = merge(...['online', 'offline'].map(eventType => fromEvent(window, eventType))).pipe(map(() => window.navigator.onLine));
+  isOnline$ = merge(...['online', 'offline'].map(eventType => fromEvent(window, eventType))).pipe(
+    startWith(),
+    map(() => window.navigator.onLine)
+  );
 }
